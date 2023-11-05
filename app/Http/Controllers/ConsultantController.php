214116ff1c26d3e09a620da8da317dbf59232cd8
4fifun <rizalniam211@gmail.com>
2023-11-05 16:22:52 +0700

    revisi consultant api

--- app/Http/Controllers/ConsultantController.php
+++ app/Http/Controllers/ConsultantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Consultant;
 use Illuminate\Http\Request;
+use Validator;
 
 class ConsultantController extends Controller
 {
