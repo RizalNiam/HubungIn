bf4e70dc1f9fc31a11c7fb3e293bd30ad2122975
RizalNiam <63841159+RizalNiam@users.noreply.github.com>
2024-01-13 12:33:01 +0700

    Update JobController.php

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -111,7 +111,7 @@ class JobController extends Controller
         ->where('province', $province)
         ->get(); 
         
-        return $this->requestSucessData('Success!', $rawData);
+        return $this->requestSuccessData('Success!', $rawData);
     }
 
     function delete_job(Request $request) {
