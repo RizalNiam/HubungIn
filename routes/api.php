a0c4badec8948aeb03f1c53afd63ff99e75c5755
RizalNiam <63841159+RizalNiam@users.noreply.github.com>
2024-01-13 12:35:48 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('jwt.verify')->group(function () {
     Route::get('auth/update_job', [JobController::class, 'update_job']);
     Route::get('auth/delete_job', [JobController::class, 'delete_job']);
     Route::get('auth/find_jobs', [JobController::class, 'find_jobs']);    
-    Route::get('auth/filter', [JobController::class, 'filter']);
+    Route::post('auth/filter', [JobController::class, 'filter']);
     Route::get('auth/get_education_jobs_desc', [JobController::class, 'get_education_jobs_desc']);
     Route::get('auth/get_education_jobs_asc', [JobController::class, 'get_education_jobs_asc']);
 
